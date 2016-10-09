6bf953f08a6ed5a54309d771bf0aec0ca12091b9
mikkerlo <admin@mikkerlo.ru>
2016-10-09 21:48:11 +0300

    Begin develop first normal version

--- FixedString.h
+++ FixedString.h
@@ -1,2 +1,14 @@
-#include <string>
-using FixedString = std::string;
+#include <vector>
+
+class FixedString {
+ public:
+    FixedString(const char* str) {
+        while (*str != '\0') {
+            data_.push_back(*str);
+        }
+        data_.push_back(*str);
+    }
+
+ private:
+    std::vector<char> data_;
+};
